41ccc916b1c7e7d59b170d23b14d26be651b627f
Helloalpacaa <helloalpacaa@gmail.com>
2024-01-29 10:58:08 -0500

    Add log in validation

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView(title: "Register",
+                           subtitle: "Start organizing todos",
+                           angle: -15,
+                           background: .orange)
+                
+                // Register form
+                Form {
+                    TextField("Full Name", text:$name)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .autocorrectionDisabled()
+                    
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                        .autocorrectionDisabled()
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    TLButton(
+                        title: "Create Account",
+                        background: .green
+                    ) {
+                        // Attempt registration
+                    }
+                    .padding()
+                }
+                .offset(y: -50)
+                    
+                Spacer()
+                
+            }
+        }
     }
 }
 
