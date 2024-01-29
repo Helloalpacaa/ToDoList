f255ddf1990d1eca4c555c28d0cf86d262b7b7ee
hpan77 <hpan77@gatech.edu>
2024-01-28 22:41:07 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by HelloAlpacaa on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
