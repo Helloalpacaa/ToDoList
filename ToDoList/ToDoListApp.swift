f0a84f8ac0186551b958a7903199b6054ea63e4c
Helloalpacaa <helloalpacaa@gmail.com>
2024-01-29 00:39:01 -0500

    Initial commit

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,10 +5,15 @@
 //  Created by HelloAlpacaa on 1/28/24.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
